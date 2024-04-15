e1e82ad56e793e6a16780e54a18557ee4d0af6e6
[Luis Alberto Mesa Martos] <[lmesamart@uoc.edu]>
2024-04-15 12:53:08 +0200

    Added files

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 // Obtener todas las miniaturas
-const miniaturas = document.querySelectorAll('.galeria-miniaturas img');
+/*const miniaturas = document.querySelectorAll('.galeria-miniaturas img');
 
 // Agregar evento clic a cada miniatura
 miniaturas.forEach(miniatura => {
@@ -13,4 +13,23 @@ miniaturas.forEach(miniatura => {
         // Cambiar la imagen grande por la imagen seleccionada
         imagenGrande.setAttribute('src', imagenGrandeURL);
     });
+});*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    var slideIndex = 0;
+    carousel();
+
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("carousel-item");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) {slideIndex = 1}
+        x[slideIndex-1].style.display = "block";
+        setTimeout(carousel, 5000); // Change image every 5 seconds
+    }
 });
+
+
