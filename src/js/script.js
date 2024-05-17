310dea335a859b90a24bb218c572fc545ad8c69d
[Luis Alberto Mesa Martos] <[lmesamart@uoc.edu]>
2024-05-17 17:47:55 +0200

    Added files PEC2

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,3 @@
-// Obtener todas las miniaturas
-/*const miniaturas = document.querySelectorAll('.galeria-miniaturas img');
-
-// Agregar evento clic a cada miniatura
-miniaturas.forEach(miniatura => {
-    miniatura.addEventListener('click', function() {
-        // Obtener la URL de la imagen grande desde el atributo data-imagen
-        const imagenGrandeURL = this.getAttribute('data-imagen');
-
-        // Obtener el elemento de la imagen grande
-        const imagenGrande = document.querySelector('.imagen-grande img');
-
-        // Cambiar la imagen grande por la imagen seleccionada
-        imagenGrande.setAttribute('src', imagenGrandeURL);
-    });
-});*/
 
 document.addEventListener("DOMContentLoaded", function() {
     var slideIndex = 0;
@@ -32,14 +16,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
-
-
-//localStorage.removeItem('hasLiked');
-//localStorage.removeItem('hasIncremented');
-//localStorage.removeItem('likes');
-//localStorage.removeItem('hasLiked2');
-//localStorage.removeItem('hasIncremented2');
-//localStorage.removeItem('likes2');
 
 function setupLikeButton(likeIconId, likeCountId) {
     //localStorage.clear();
